c5afae0396592cb81bf665afa15964bcf5228f17
Tobias Gozzi <tobias.gozzi@me.com>
2016-04-27 21:38:50 +0200

    Initial Commit

---
+++ myLittleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myLittleMonster
+//
+//  Created by Tobias Gozzi on 27.04.16.
+//  Copyright © 2016 Tobias Gozzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
